7a1dc548042ed8a822f6f0407351e52327c31b5b
petergu <guyimin@mail.ustc.edu.cn>
2020-10-21 20:27:10 +0800

    PSRAM controller first version, w/o testing though

--- pCPU/pCPU.srcs/sources_1/new/psram_controller.v
+++ pCPU/pCPU.srcs/sources_1/new/psram_controller.v
@@ -17,7 +17,7 @@ module psram_controller(
     input clk_pulse_slow, 
 
     output reg ce = 1, 
-    output mosi = 0, 
+    output mosi, 
     input miso, 
     output sclk = 0, 
 
@@ -33,8 +33,7 @@ module psram_controller(
 
     output ready,
 
-    output reg [4:0]state, 
-    output reg [7:0]recv_data
+    output reg [4:0]state
 );
 
 	localparam INIT = 0;
@@ -44,29 +43,33 @@ module psram_controller(
 	localparam IDLE = 4;
 	localparam SEND = 5;
 	localparam READING = 6;
-	localparam WRITING = 7;
+	localparam WRITING_PREPARE = 7;
+	localparam WRITING = 8;
 
 	reg [4:0]state_return;
 	reg [15:0]boot_counter = 35;
-	reg [31:0]address;
 	reg [31:0]sendbits;
 	reg [7:0]sendcnt;
-	reg sendmode; // 1: send cmd, 0: send data
+	//reg sendmode; // 1: send cmd, 0: send data
 
-	reg [7:0]recvbits;
-	reg [2:0]recvcnt; // auto-overflow from 7 to 0
+	reg [7:0]databyte;
+	reg [2:0]bytecnt; // auto-overflow from 7 to 0
+
+    assign mosi = (state ==READING | state == WRITING) ?
+		sendbits[31] : databyte[7];
+    assign ready = (state == IDLE);
 
 	reg rend_latch = 0;
 	always @ (posedge clk) begin
-		if (rst) rend_latch = 0;
+		if (rst) rend_latch <= 0;
 		else if (state == READING & rend) rend_latch <= 1;
-		else if (state != READING) rend_latch = 0;
+		else if (state != READING) rend_latch <= 0;
 	end
 	reg wend_latch = 0;
 	always @ (posedge clk) begin
-		if (rst) wend_latch = 0;
+		if (rst) wend_latch <= 0;
 		else if (state == READING & wend) wend_latch <= 1;
-		else if (state != READING) wend_latch = 0;
+		else if (state != READING) wend_latch <= 0;
 	end
 
     always @(posedge clk) begin
@@ -75,6 +78,8 @@ module psram_controller(
 			sclk <= 0;
 			boot_counter <= 35;
 			state <= INIT;
+			byte_available <= 0;
+			ready_for_next_byte <= 0;
         end
         else if (clk_pulse_slow) case(state)
 				// initialization >150us, CLK low, CE# high
@@ -85,7 +90,6 @@ module psram_controller(
 				RSTEN: begin
 					sendbits <= {8'b01100110, 24'b0};
 					sendcnt <= 8;
-					sendmode <= 1;
 					ce <= 0;
 					state <= SEND;
 					state_return <= BTWN_RST;
@@ -102,58 +106,75 @@ module psram_controller(
 					state_return <= IDLE;
 				end
                 IDLE: begin
-					ce <= 1;
 					sclk <= 0;
 					if (we) begin
-						address <= a;
 						sendbits <= {8'b0000010, a[23:0]};
 						sendcnt <= 8 + 24;
 						state <= SEND;
-						state_return <= WRITING;
+						state_return <= WRITING_PREPARE;
+						bytecnt <= 0;
+						ce <= 0;
 					end
 					else if (rd) begin
-						address <= a;
 						sendbits <= {8'b0000011, a[23:0]};
 						sendcnt <= 8 + 24;
 						state <= SEND;
 						state_return <= READING;
+						bytecnt <= 0;
+						ce <= 0;
 					end
+					else ce <= 1;
                 end
 				SEND: begin
 					// the first bit is automatically prepared
 					// before state <= SEND:
 					// sclk should be zero, ce should be 0
+					// ce will be kept 0 after state_return
 					sclk <= ~sclk;
 					if (sclk) begin
 						sendcnt <= sendcnt - 1;
 						sendbits <= {sendbits[30:0], 1'b1};
 						if (sendcnt == 1) begin
-							ce <= 1;
 							state <= state_return;
+							if (state_return == WRITING_PREPARE)
+								ready_for_next_byte <= 1;
 						end
 					end
 				end
 				READING: begin
-					if (rend_latch & sclk == 0 & recvcnt == 0) begin
+					if (rend_latch & sclk == 0 & bytecnt == 0) begin
 						state <= IDLE;
 						ce <= 1;
 					end
 					else sclk <= ~sclk;
 					if (sclk) begin
-						recvcnt <= recvcnt + 1;
-						recvbits <= {recvbits[6:0], miso};
-						if (recvcnt == 7) begin
+						bytecnt <= bytecnt + 1;
+						databyte <= {databyte[6:0], miso};
+						if (bytecnt == 7) begin
 							byte_available <= 1;
-							dout <= {recvbits[6:0], miso};
+							dout <= {databyte[6:0], miso};
 						end
 						else byte_available <= 0;
 					end
 				end
+				WRITING_PREPARE: begin
+					databyte <= din;
+					ready_for_next_byte <= 0;
+				end
 				WRITING: begin
+					sclk <= ~sclk;
+					if (sclk) begin
+						databyte <= bytecnt == 7 ?
+							din : {databyte[6:0], 1'b1};
+						bytecnt <= bytecnt + 1;
+						if (bytecnt == 6) ready_for_next_byte <= 1;
+						else ready_for_next_byte <= 0;
+						if (wend_latch & bytecnt == 7) begin
+							state <= IDLE;
+							ce <= 1;
+						end
+					end
 				end
 		endcase
     end
-
-    assign mosi = sendmode ? sendbits[31] : data_sig[7];
-    assign ready = (state == IDLE);
 endmodule
